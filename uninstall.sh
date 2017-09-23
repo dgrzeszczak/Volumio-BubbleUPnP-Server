e86990707bdea63851f4e213bd23504149b5080d
dgrzeszczak <dariusz.grzeszczak@interia.pl>
2017-09-23 13:26:09 +0200

    Update uninstall.sh
    remove plugin directory

--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,7 @@
 # Uninstall dependendencies
 # apt-get remove -y
 
+sudo rm -rf /data/plugins/miscellanea/volumio_bubbleupnp_server/
 sudo /opt/bin/ipkg remove  bubbleupnpserver-installer
 
 echo "Done"
